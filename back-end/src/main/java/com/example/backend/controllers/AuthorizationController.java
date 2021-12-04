4ab0e5fc09d646430d59d814ad29b056030977e5
Daniil <twilightnotch@gmail.com>
2021-12-04 16:02:46 +0300

    new key

--- back-end/src/main/java/com/example/backend/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/example/backend/controllers/AuthorizationController.java
@@ -1,9 +1,11 @@
 package com.example.backend.controllers;
 
+
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +17,23 @@ import java.io.IOException;
 @RequestMapping("/auth")
 public class AuthorizationController {
     private String cardNumber;
+
+    @GetMapping("/tokenApi")
+    public String getTokenApi() throws IOException, InterruptedException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://epa.api.vtb.ru/vtb/openapi/id.vtb.ru/oauth2/authorize?redirect_uri=https://vk.com/&state=FDsdfsS&client_id=5445654378901234&response_type=code")
+                .get()
+                .addHeader("Authorization", "Bearer {AQIC5wM2LY4SfcyBy2fw4_k-7N4eWXik7wtV_T15gjXhrRM.*AAJTSQACMDIAAlNLABI3NTM0NzYwNDQ4NTA5NzgwODQAAlMxAAIwMQ..*}")
+                .addHeader("accept", "application/json")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.toString());
+        return response.toString();
+    }
+
     @PostMapping("/token")
     public String getToken() throws IOException{
         OkHttpClient client = new OkHttpClient();
@@ -25,7 +44,7 @@ public class AuthorizationController {
         Request request = new Request.Builder()
                 .url("https://epa.api.vtb.ru/vtb/openapi/id.vtb.ru/oauth2/token")
                 .post(body)
-                .addHeader("Authorization", "Bearer {token}")
+                .addHeader("Authorization", "Bearer {AQIC5wM2LY4SfcxO-qS9OacuR7WT0UASwsau4UNKpsF0mSE.*AAJTSQACMDIAAlNLABQtNjc2ODMyMTg2MTA4MDkyNzkxOAACUzEAAjAx*}")
                 .addHeader("content-type", "application/json")
                 .addHeader("accept", "application/json")
                 .build();
@@ -41,7 +60,7 @@ public class AuthorizationController {
         Request request = new Request.Builder()
                 .url("https://epa.api.vtb.ru/vtb/openapi/id.vtb.ru/oauth2/authorize?redirect_uri=https://vk.com/&state=FDsdfsS&client_id=5445654378901234&response_type=code")
                 .get()
-                .addHeader("Authorization", "Bearer {AQIC5wM2LY4SfcxjIMtc7JUj2_-LM3exxfbgqbbX5AQcCvE.*AAJTSQACMDIAAlNLABMzNTQwOTA3NzQ1Njc4Nzg5NTUzAAJTMQACMDE.*}")
+                .addHeader("Authorization", "Bearer {AQIC5wM2LY4SfcyBy2fw4_k-7N4eWXik7wtV_T15gjXhrRM.*AAJTSQACMDIAAlNLABI3NTM0NzYwNDQ4NTA5NzgwODQAAlMxAAIwMQ..*}")
                 .addHeader("accept", "application/json")
                 .build();
 
