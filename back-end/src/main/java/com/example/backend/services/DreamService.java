e2f60275dff8c4191cb66c7553dfb10afb409ff3
bonchEquipment <arsenydevyatkin@gmail.com>
2021-12-08 21:38:19 +0300

    Remade class for new Repository

--- back-end/src/main/java/com/example/backend/services/DreamService.java
+++ back-end/src/main/java/com/example/backend/services/DreamService.java
@@ -1,20 +1,21 @@
 package com.example.backend.services;
 
+
 import com.example.backend.entyties.DreamEntity;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.cloud.FirestoreClient;
+import com.example.backend.repositories.DreamEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
+
 @Service
 public class DreamService {
-    private static final String COLLECTION_NAME = "dreams";
+
+    @Autowired
+    DreamEntityRepository dreamEntityRepository;
+
     public String saveDream(DreamEntity dreamEntity) throws ExecutionException, InterruptedException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> collectionApiFuture =dbFirestore.collection(COLLECTION_NAME)
-                .document(dreamEntity.getNameDream()).set(dreamEntity);
-        return collectionApiFuture.get().getUpdateTime().toString();
+        dreamEntityRepository.save(dreamEntity);
+        return "success";
     }
 }
