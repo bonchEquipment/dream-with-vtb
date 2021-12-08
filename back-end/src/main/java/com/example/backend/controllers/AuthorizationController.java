81e625ee318341bf899050ed4716da1eec86674f
Daniil <twilightnotch@gmail.com>
2021-12-08 21:20:27 +0300

    aa

--- back-end/src/main/java/com/example/backend/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/example/backend/controllers/AuthorizationController.java
@@ -27,38 +27,19 @@ public class AuthorizationController {
         return userService.getVTBTokens().toString();
     }
 
-    @PostMapping("/token")
-    public String getToken() throws IOException{
-        OkHttpClient client = new OkHttpClient();
-
-        MediaType mediaType = MediaType.parse("application/json");
-
-        com.squareup.okhttp.RequestBody body = com.squareup.okhttp.RequestBody.create(mediaType,
-                "{\"grant_type\":\"code\",\"code\":\"abuwacihiebinuzr\",\"refresh_token\":\"0f9665541d8815fdf7da9fd22f437a4544a000c22e40fbeb5af624efdf24be83\"}");
-        Request request = new Request.Builder()
-                .url("https://epa.api.vtb.ru/vtb/openapi/id.vtb.ru/oauth2/token")
-                .post(body)
-                .addHeader("Authorization", "Bearer {token}")
-                .addHeader("content-type", "application/json")
-                .addHeader("accept", "application/json")
-                .build();
-
-        Response response = client.newCall(request).execute();
-        System.out.println(response);
-        return response.toString();
-    }
 
     @GetMapping("/login")
-    public String login() throws IOException {
+    public ResponseEntity<?> login() throws IOException, InterruptedException {
+        String token = getTokenApi();
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://epa.api.vtb.ru/vtb/openapi/id.vtb.ru/oauth2/authorize?redirect_uri=https://vk.com/&state=FDsdfsS&client_id=5445654378901234&response_type=code")
                 .get()
-                .addHeader("Authorization", "Bearer {token}")
+                .addHeader("Authorization", "Bearer "+token)
                 .addHeader("accept", "application/json")
                 .build();
         Response response = client.newCall(request).execute();
         System.out.println(response.toString());
-        return response.toString();
+        return ResponseEntity.ok().body(response);
     }
 }
