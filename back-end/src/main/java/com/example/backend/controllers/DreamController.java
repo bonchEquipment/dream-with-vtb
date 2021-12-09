d3506a12761581b73d477f64c11fc6a6102bf70e
bonchEquipment <arsenydevyatkin@gmail.com>
2021-12-09 15:41:57 +0300

    Added updateDream method

--- back-end/src/main/java/com/example/backend/controllers/DreamController.java
+++ back-end/src/main/java/com/example/backend/controllers/DreamController.java
@@ -4,22 +4,27 @@ import com.example.backend.entyties.DreamEntity;
 import com.example.backend.services.DreamService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/dream")
+@RequestMapping("/dreams")
 public class DreamController {
     @Autowired
     private DreamService dreamService;
-    @PostMapping("/dreams")
-    public String saveProduct(@RequestBody DreamEntity dreamEntity) throws ExecutionException, InterruptedException {
-        System.out.println(dreamEntity.toString());
-        return dreamService.saveDream(dreamEntity);
-        // здесь не понятно что, кажется надо менять, тестовая штука
+
+
+    @PostMapping()
+    public String saveDream(@RequestBody DreamEntity dreamEntity) throws ExecutionException, InterruptedException {
+        dreamService.saveDream(dreamEntity);
+        return "dream was successfully created";
     }
+
+    @PutMapping("{id}")
+    public String updateDream(@PathVariable("id") long id,@RequestBody DreamEntity dreamEntity){
+        return dreamService.updateDream(id, dreamEntity);
+    }
+
+
 }
