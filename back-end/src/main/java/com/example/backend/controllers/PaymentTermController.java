692d4132d9f951b79b08094ce9e37ff441df8e49
Daniil <twilightnotch@gmail.com>
2021-12-09 13:29:37 +0300

    Работающий расчет суммы

--- back-end/src/main/java/com/example/backend/controllers/PaymentTermController.java
+++ back-end/src/main/java/com/example/backend/controllers/PaymentTermController.java
@@ -5,10 +5,7 @@ import com.example.backend.services.UserService;
 import com.squareup.okhttp.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pay")
@@ -18,8 +15,8 @@ public class PaymentTermController {
     private PaymentTermService paymentTermService;
 
     //вместе запросом передаем :накопленную сумму, сумму которую нужно накопить, и сумму которую откладываем ежемесячно
-    @PostMapping("/payment")
-    public ResponseEntity<?> getPaymentTerm(@RequestBody int sum, @RequestBody int maxSum, @RequestBody int salary){
+    @GetMapping("/payment")
+    public ResponseEntity<?> getPaymentTerm(@RequestParam Integer sum,@RequestParam Integer maxSum, @RequestParam Integer salary){
         String response = paymentTermService.getTerm(sum,maxSum,salary);
         return ResponseEntity.ok().body(response);
     }
--- back-end/src/main/java/com/example/backend/services/PaymentTermService.java
+++ back-end/src/main/java/com/example/backend/services/PaymentTermService.java
@@ -7,7 +7,7 @@ public class PaymentTermService {
     //Метод для расчета того, сколько осталось накопить на мечту
     public String getTerm(int sum, int maxSum, int salary) {
         int term = (int) (maxSum - sum) / salary;
-        String date = null;
+        String date = "";
         if (term > 12) {
             if (term / 12 == 1) {
                 date = Integer.toString(term / 12) + " год ";
@@ -19,17 +19,18 @@ public class PaymentTermService {
             }
             if (term / 12 >= 5 && term / 12 <= 20) {
                 date = Integer.toString(term / 12) + " лет ";
+                term = term - (term / 12) * 12;
             }
         }
         if (term <= 12) {
             if (term == 1) {
-                date = Integer.toString(term) + " месяц";
+                date += Integer.toString(term) + " месяц";
             }
             if (term >= 2 && term <= 4) {
-                date = Integer.toString(term) + " месяца";
+                date += Integer.toString(term) + " месяца";
             }
             if (term <= 12 && term >= 5) {
-                date = Integer.toString(term) + " месяцев";
+                date += Integer.toString(term) + " месяцев";
             }
         }
 
