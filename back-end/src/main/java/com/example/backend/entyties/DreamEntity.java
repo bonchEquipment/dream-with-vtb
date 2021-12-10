14582026a00a5039411006b29f12610e3097617a
bonchEquipment <arsenydevyatkin@gmail.com>
2021-12-10 13:25:14 +0300

    Added field for the amount of money to save each month, getters, setters, constructor, hashcode, equals

--- back-end/src/main/java/com/example/backend/entyties/DreamEntity.java
+++ back-end/src/main/java/com/example/backend/entyties/DreamEntity.java
@@ -1,6 +1,7 @@
 package com.example.backend.entyties;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity(name = "dreams")
 public class DreamEntity {
@@ -12,26 +13,29 @@ public class DreamEntity {
     private String dreamName = null;
     private int userId;
     private int priorityLevel;
+    private int amountOfMoneyToSaveEachMonth;
 
 
     public DreamEntity() {
     }
 
-    public DreamEntity(Long dreamId, Integer moneyToDream, String description, String dreamName, int userId, int priorityLevel) {
+    public DreamEntity(Long dreamId, Integer moneyToDream, String description, String dreamName, int userId, int priorityLevel, int amountOfMoneyToSaveEachMonth) {
         this.dreamId = dreamId;
         this.moneyToDream = moneyToDream;
         this.description = description;
         this.dreamName = dreamName;
         this.userId = userId;
         this.priorityLevel = priorityLevel;
+        this.amountOfMoneyToSaveEachMonth = amountOfMoneyToSaveEachMonth;
     }
 
-    public DreamEntity(Integer moneyToDream, String description, String dreamName, int userId, int priorityLevel) {
+    public DreamEntity(Integer moneyToDream, String description, String dreamName, int userId, int priorityLevel, int amountOfMoneyToSaveEachMonth) {
         this.moneyToDream = moneyToDream;
         this.description = description;
         this.dreamName = dreamName;
         this.userId = userId;
         this.priorityLevel = priorityLevel;
+        this.amountOfMoneyToSaveEachMonth = amountOfMoneyToSaveEachMonth;
     }
 
     public Integer getMoneyToDream() {
@@ -50,6 +54,8 @@ public class DreamEntity {
                 ", description='" + description + '\'' +
                 ", dreamName='" + dreamName + '\'' +
                 ", userId=" + userId +
+                ", priorityLevel=" + priorityLevel +
+                ", amountOfMoneyToSaveEachMonth=" + amountOfMoneyToSaveEachMonth +
                 '}';
     }
 
@@ -91,5 +97,32 @@ public class DreamEntity {
 
     public void setDreamName(String nameDream) {
         this.dreamName = nameDream;
+    }
+
+    public int getAmountOfMoneyToSaveEachMonth() {
+        return amountOfMoneyToSaveEachMonth;
+    }
+
+    public void setAmountOfMoneyToSaveEachMonth(int amountOfMoneyToSaveEachMonth) {
+        this.amountOfMoneyToSaveEachMonth = amountOfMoneyToSaveEachMonth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DreamEntity)) return false;
+        DreamEntity that = (DreamEntity) o;
+        return userId == that.userId &&
+                priorityLevel == that.priorityLevel &&
+                amountOfMoneyToSaveEachMonth == that.amountOfMoneyToSaveEachMonth &&
+                Objects.equals(dreamId, that.dreamId) &&
+                Objects.equals(moneyToDream, that.moneyToDream) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(dreamName, that.dreamName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dreamId, moneyToDream, description, dreamName, userId, priorityLevel, amountOfMoneyToSaveEachMonth);
     }
 }
