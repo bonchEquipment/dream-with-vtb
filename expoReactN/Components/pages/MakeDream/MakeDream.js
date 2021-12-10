c646c4d7c854e0b6a04ecf6c8b2d6d6597fba26e
Andrey Vsiliev <filiston431@gmail.com>
2021-12-10 21:43:03 +0300

    add make dream page

--- expoReactN/Components/pages/MakeDream/MakeDream.js
+++ expoReactN/Components/pages/MakeDream/MakeDream.js
@@ -1,11 +1,12 @@
 import {Text, TouchableHighlight, View, StyleSheet, Image} from "react-native";
 import React from "react";
+import FormMake from "./FormMake";
 
 export default function MakeDream(){
     return(
         <View style={styles.element} >
            {/*<Text>Извините, не успели. Но на бэке логика есть.</Text>*/}
-
+            <FormMake/>
         </View>
     )
 }
@@ -14,5 +15,5 @@ const styles= StyleSheet.create({
     element:{
         padding:15,
         flexDirection: "row",
-    },
+    }
 })
